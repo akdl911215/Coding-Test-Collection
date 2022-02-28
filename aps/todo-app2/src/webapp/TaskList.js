19c41f8e5603a6d61fae76bf477656090ec2d8c4
akdl911215 <akdl911215@gmail.com>
2022-02-28 11:41:06 +0900

    testing

--- aps/todo-app2/src/webapp/TaskList.js
+++ aps/todo-app2/src/webapp/TaskList.js
@@ -2,15 +2,6 @@ import React, { useState } from "react";
 
 const TaskList = (props) => {
   const { tasks } = props;
-  console.log("🚀 >> tasks", tasks);
-  const [id, setId] = useState(0);
-  const [checkBool, setCheckBool] = useState(false);
-
-  const handleSubmit = (e) => {
-    console.log("E :  ", e);
-
-    props.onSubmit({ done: !e.target.checked });
-  };
 
   return (
     <>
@@ -19,7 +10,6 @@ const TaskList = (props) => {
           return (
             <>
               <li key={task.id} style={{ marginBottom: 8 }}>
-                <input type="checkbox" onClick={handleSubmit} />
                 <span>{task.title}</span>
               </li>
             </>
